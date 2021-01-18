e4b4d5a245942d0c1d0377755be179e7c1327931
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-01-18 16:16:13 +0900

    fixed em-algorhythm

--- em-algorhythm.py
+++ em-algorhythm.py
@@ -1,70 +1,81 @@
-#numpy,scipy.statsからnorm,math,matplotlib.pyplotをインポート！
 import numpy as np
 from scipy.stats import norm
-ball_pattern_arr = [8,3,6,5,7,3,6,4]
-PA = 1/2
-PB = 1/2
+ball_weight_pattern_arr = [8,3,6,5,7,3,6,4]
+Pa = 1/2
+Pb = 1/2
 
+# TODO: to Class
 
+# TODO:型付け
 Ma = {"m":6,"sigma":1}
 Mb = {"m":4,"sigma":1}
 
-def get_propbability(num,dist_param):
-    return norm.pdf(num,dist_param["m"],dist_param["sigma"])
+def get_ball_propbability_tsubo(ball_weight,tsubo_param):
+    return norm.pdf(ball_weight,tsubo_param["m"],tsubo_param["sigma"])
 
-def P_num(num,Ma=Ma,Mb=Mb):
-    return norm.pdf(num,Ma["m"],Ma["sigma"]) + norm.pdf(num,Mb["m"],Mb["sigma"])
+def get_tsubo_conditional_probability_by_ball_weight(ball_weight,tsubo_param,P):
+    return  P * get_ball_propbability_tsubo(ball_weight,tsubo_param) / (Pa * get_ball_propbability_tsubo(ball_weight,Ma) + Pb * get_ball_propbability_tsubo(ball_weight,Mb))
 
-def get_propbability_sum(dist_param,pattern_arr):
-    probability_sum = 0.0
-    for pattern in pattern_arr:
-        probability_sum += get_propbability(pattern,dist_param)/P_num(pattern)
-    return probability_sum
+def get_conditional_propbability_sum(ball_weight_pattern_arr,tsubo_param,P):
+    return sum([get_tsubo_conditional_probability_by_ball_weight(ball_weight,tsubo_param,P) for ball_weight in ball_weight_pattern_arr])
 
-def get_updated_probability_dist(dist_param,pattern_arr):
-    probability_sum = 0.0
-    for pattern in pattern_arr:
-        probability_sum += get_propbability(pattern,dist_param)/P_num(pattern)
-    return probability_sum / len(pattern_arr)
+def get_updated_probability_to_choose(ball_weight_pattern_arr,tsubo_param,P):
+    n = len(ball_weight_pattern_arr)
+    return get_conditional_propbability_sum(ball_weight_pattern_arr,tsubo_param,P) / n
 
-def get_updated_probability_ave(dist_param,pattern_arr):
-    probability_sum = 0.0
-    for pattern in pattern_arr:
-        probability_sum += get_propbability(pattern,dist_param)/P_num(pattern) * pattern
-    return probability_sum / get_propbability_sum(dist_param,pattern_arr)
+def get_updated_tsubo_average(ball_weight_pattern_arr,tsubo_param,P):
+    return sum([get_tsubo_conditional_probability_by_ball_weight(ball_weight,tsubo_param,P) * ball_weight for ball_weight in ball_weight_pattern_arr]) / get_conditional_propbability_sum(ball_weight_pattern_arr,tsubo_param,P)
 
-def get_updated_probability_sigma(dist_param,pattern_arr):
-    probability_sum = 0.0
-    ave = get_updated_probability_ave(dist_param,pattern_arr)
-    for pattern in pattern_arr:
-        probability_sum += (get_propbability(pattern,dist_param)/P_num(pattern)) * (pattern - ave) ** 2
-    return probability_sum / get_propbability_sum(dist_param,pattern_arr)
+def get_updated_tsubo_sigma(ball_weight_pattern_arr,tsubo_param,P):
+    average = get_updated_tsubo_average(ball_weight_pattern_arr,tsubo_param,P)
+    return sum([get_tsubo_conditional_probability_by_ball_weight(ball_weight,tsubo_param,P) * (ball_weight - average) ** 2 for ball_weight in ball_weight_pattern_arr]) / get_conditional_propbability_sum(ball_weight_pattern_arr,tsubo_param,P)
 
-    
+def get_updated_tsubo_param(ball_weight_pattern_arr,tsubo_param,P):
+    return {"m":get_updated_tsubo_average(ball_weight_pattern_arr,tsubo_param,P),"sigma":get_updated_tsubo_sigma(ball_weight_pattern_arr,tsubo_param,P)}    
+
 
 # T1
-print('P(X = 4|MA) = {:.2f}'.format(norm.pdf(4,Ma["m"],Ma["sigma"])))
-print('P(X = 4|MB) = {:.2f}'.format(norm.pdf(4,Mb["m"],Mb["sigma"])))
-print(get_propbability(4,Ma))
+print('P(X = 4|MA) = {:.2f}'.format(get_ball_propbability_tsubo(4,Ma)))
+print('P(X = 4|MB) = {:.2f}'.format(get_ball_propbability_tsubo(4,Mb)))
 
-print('P(Y = A|X = 4,M) = {:.2f}'.format((norm.pdf(4,Ma["m"],Ma["sigma"]) )/P_num(4)))
-print('P(Y = B|X = 4,M) = {:.2f}'.format((norm.pdf(4,Mb["m"],Mb["sigma"]) )/P_num(4)))
+print('P(Y = A|X = 4,M) = {:.2f}'.format(get_tsubo_conditional_probability_by_ball_weight(4,Ma,Pa)))
+print('P(Y = B|X = 4,M) = {:.2f}'.format(get_tsubo_conditional_probability_by_ball_weight(4,Mb,Pb)))
 
-print('P(X = 5|MA) = {:.2f}'.format(norm.pdf(5,Ma["m"],Ma["sigma"])))
-print('P(X = 5|MB) = {:.2f}'.format(norm.pdf(5,Mb["m"],Mb["sigma"])))
+print('P(X = 5|MA) = {:.2f}'.format(get_ball_propbability_tsubo(5,Ma)))
+print('P(X = 5|MB) = {:.2f}'.format(get_ball_propbability_tsubo(5,Mb)))
 
-print('P(Y = A|X = 5,M) = {:.2f}'.format((norm.pdf(5,Ma["m"],Ma["sigma"]))/P_num(5)))
-print('P(Y = B|X = 5,M) = {:.2f}'.format((norm.pdf(5,Mb["m"],Mb["sigma"]))/P_num(5)))
-
-
-print(get_updated_probability_dist(Ma,ball_pattern_arr))
-print(get_updated_probability_dist(Mb,ball_pattern_arr))
-print(get_updated_probability_ave(Ma,ball_pattern_arr))
-print(get_updated_probability_ave(Mb,ball_pattern_arr))
-print(get_updated_probability_sigma(Ma,ball_pattern_arr))
-print(get_updated_probability_sigma(Mb,ball_pattern_arr))
-
-# for 分で回せたら、幸せ
+print('P(Y = A|X = 5,M) = {:.2f}'.format(get_tsubo_conditional_probability_by_ball_weight(5,Ma,Pa)))
+print('P(Y = B|X = 5,M) = {:.2f}'.format(get_tsubo_conditional_probability_by_ball_weight(5,Mb,Pb)))
 
 
+print(get_updated_probability_to_choose(ball_weight_pattern_arr,Ma,Pa))
+print(get_updated_probability_to_choose(ball_weight_pattern_arr,Mb,Pb))
 
+print(get_updated_tsubo_average(ball_weight_pattern_arr,Ma,Pa))
+print(get_updated_tsubo_average(ball_weight_pattern_arr,Mb,Pb))
+
+print(get_updated_tsubo_sigma(ball_weight_pattern_arr,Ma,Pa))
+print(get_updated_tsubo_sigma(ball_weight_pattern_arr,Mb,Pb))
+
+
+MAX_ITER = 10
+
+def round_dict_val(dict,round_on=2):
+    round_dict = {}
+    for item in dict:
+        round_dict[item] = round(dict[item],round_on)
+    return round_dict
+
+for i in range(MAX_ITER):
+    print("----------{}-----------".format(i))
+    print("Ma:{}".format(Ma))
+    print("Mb:{}".format(Mb))
+    print("Pa:{}".format(Pa))
+    print("Pb:{}".format(Pb))
+    print("-----------------------".format(i))
+    Ma_temp = get_updated_tsubo_param(ball_weight_pattern_arr,Ma,Pa)
+    Mb_temp = get_updated_tsubo_param(ball_weight_pattern_arr,Mb,Pb)
+    Pa_temp = get_updated_probability_to_choose(ball_weight_pattern_arr,Ma,Pa)
+    Pb_temp = get_updated_probability_to_choose(ball_weight_pattern_arr,Mb,Pb)
+    Ma,Mb = round_dict_val(Ma_temp,2),round_dict_val(Mb_temp,2)
+    Pa,Pb = round(Pa_temp,2),round(Pb_temp,2)
